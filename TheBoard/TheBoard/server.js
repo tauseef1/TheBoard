dc109bb7f84c8333aa3ed465cf8d774a724c0b28
vanhouc <vanhouc@mail.nmc.edu>
2014-05-01 16:55:28 -0400

    Added basic server

--- TheBoard/TheBoard/server.js
+++ TheBoard/TheBoard/server.js
@@ -1,1 +1,9 @@
-﻿
+﻿var http = require("http");
+
+var server = http.createServer(function (req,res) {
+    console.log(req.url);
+    res.write("<html><body><h1>" + req.url + "</h1></body></html>");
+    res.end();
+    });
+
+server.listen(1337);
