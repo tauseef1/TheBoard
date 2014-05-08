6ee2ce3b53d7ec55add0e5a9f3b642db4946ef5d
vanhouc <vanhouc@mail.nmc.edu>
2014-05-08 16:05:18 -0400

    Commit darn you!!!

---
+++ TheBoard/TheBoard/server.js
@@ -0,0 +1,22 @@
+﻿var http = require( "http" );
+var express = require( "express" );
+var app = express();
+var controllers = require( "./controllers" );
+
+// Setup the View Engine
+app.set( "view engine", "jade" );
+
+//static resource folder
+app.use( express.static( __dirname + "/public" ) );
+
+// Map the routes
+controllers.init( app );
+
+app.get( "/api/users", function ( req, res ) {
+    res.set( "Content-Type", "application/json" );
+    res.send( { name: "Shawn", isValid: true, group: "Admin" });
+});
+
+var server = http.createServer( app );
+
+server.listen( 1337 );
