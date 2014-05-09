1ef1fd6ace5c6827d0812398e8e47971471835d3
vanhouc <vanhouc@mail.nmc.edu>
2014-05-09 13:38:12 -0400

    More of a mess

---
+++ TheBoard/TheBoard/controllers/notesController.js
@@ -0,0 +1,43 @@
+﻿( function ( notesController ) {
+
+    var data = require( "../data" );
+
+    notesController.init = function ( app ) {
+
+        app.get( "/api/notes/:categoryName", function ( req, res ) {
+
+            var categoryName = req.params.categoryName;
+
+            data.getNotes( categoryName, function ( err, notes ) {
+                if ( err ) {
+                    res.send( 400, err );
+                }
+                res.set( "Content-Type", "application/json" );
+                res.send( notes.notes );
+            });
+        });
+
+        app.post( "/api/notes/:categoryName", function ( req, res ) {
+
+            var categoryName = req.params.categoryName;
+
+            var noteToInsert = {
+                note: req.body.note,
+                color: req.body.color,
+                author: "Cameron VanHouzen"
+            };
+            console.log( "Posted new note: " + req.body.note + " " + req.body.color );
+            data.addNote( categoryName, noteToInsert, function ( err ) {
+                if ( err ) {
+                    res.send( 400, "Failed to add note to data store" );
+                }
+                else {
+                    res.set( "Content-Type", "application/json" );
+                    res.send(201, noteToInsert);
+                }
+            });
+
+        });
+
+    };
+})( module.exports );
